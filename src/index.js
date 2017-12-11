cef0bb7700d83931ce4b614da0be3215eb1b2bd4
Faris Mustafa <farismmk@gmail.com>
2017-12-10 18:28:06 -0800

    override menuicon partial

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { useCustomPartials, pageLoader, Styleguider } from 'styleguider'
 
 import logo from './assets/logo.png'
 import Logo from './partials/Logo'
+import MenuIcon from './partials/MenuIcon'
 import Button from './react/Button'
 
 const sections = [
   {
+    icon: 'introduction',
     title: 'Introduction',
     sections: [
       {
@@ -17,7 +19,22 @@ const sections = [
     ],
   },
   {
+    icon: 'elements',
     title: 'Elements',
+    sections: [
+      {
+        title: 'Avatar',
+        loader: pageLoader(() => import('./sections/elements/avatar.md')),
+      },
+      {
+        title: 'Button',
+        loader: pageLoader(() => import('./sections/elements/button.md')),
+      },
+    ],
+  },
+  {
+    icon: 'layouts',
+    title: 'Layouts',
     sections: [
       {
         title: 'Avatar',
@@ -35,6 +52,7 @@ const components = { Button }
 
 const partials = useCustomPartials({
   logo: Logo,
+  menuIcon: MenuIcon,
 })
 
 ReactDOM.render(
